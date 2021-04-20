c35e715196c631a232178a83ffb64e5bd99c4e30
Daniel Martinez <dm050471@gmail.com>
2021-04-20 15:13:41 -0600

    Cambios2

--- Ejercicio/ProyectoFinal/Habitacion.cs
+++ Ejercicio/ProyectoFinal/Habitacion.cs
@@ -4,4 +4,12 @@ public class Habitacion
     public string TipoHabitacion { get; set; }
     public int NCamas { get; set; }
     public double Precio { get; set; }
+
+    public Habitacion(int codigo, string tipohabitacio, int ncamas, double precio)
+    {
+        Codigo = codigo;
+        TipoHabitacion = tipohabitacio;
+        NCamas = ncamas;
+        Precio = precio;
+    }
 }
--- Ejercicio/ProyectoFinal/Reservacion.cs
+++ Ejercicio/ProyectoFinal/Reservacion.cs
@@ -7,10 +7,14 @@ public class Reservacion
     public DateTime Fecha { get; set; }
     public int NumeroPersonas { get; set; }
     public string NReservacion { get; set; }
+    public double Precio { get; set; }
     public Cliente Cliente { get; set; }
     public Recepcionista Recepcionista { get; set; }
+    public Habitacion Habitacion { get; set; }
 
-    public Reservacion(int codigoreservacion, DateTime fecha, int numeropersonas, string nreservacion, Cliente cliente, Recepcionista recepcionista)
+    public List <Reservaciondetalle> Reservaciondetalle { get; set; }
+
+    public Reservacion(int codigoreservacion, DateTime fecha, int numeropersonas, string nreservacion, Cliente cliente, Recepcionista recepcionista, Habitacion habitacion)
     {
         CodigoReservacion = codigoreservacion;
         Fecha = fecha;
@@ -18,6 +22,17 @@ public class Reservacion
         NReservacion = nreservacion;
         Cliente = cliente;
         Recepcionista = recepcionista;
+        Precio = habitacion.Precio;
+        Reservaciondetalle = new List<Reservaciondetalle>();
+    }
+
+    public void NuevaReservacion(Habitacion habitacion)
+    {
+        int nuevahabitacion = Reservaciondetalle.Count + 1;
+        int cantidad = 1;
+
+        Reservaciondetalle o = new  Reservaciondetalle(nuevahabitacion, 1, habitacion);
+        Reservaciondetalle.Add(o);
     }
 
 }
